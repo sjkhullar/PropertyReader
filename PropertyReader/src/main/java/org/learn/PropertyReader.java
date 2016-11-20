c38f834dd124153c068ac6e1736d5dc0f39105e7
makeinjava <makeinjava@gmail.com>
2016-11-20 23:25:18 +0530

    Read property file from resource directory using java (with example)

---
+++ PropertyReader/src/main/java/org/learn/PropertyReader.java
@@ -0,0 +1,34 @@
+package org.learn;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertyReader {
+
+    public static void main(String[] args) throws IOException {
+        readProperties();
+    }
+
+    private static void readProperties() throws IOException {
+        InputStream inputStream = null;
+        try {
+            Properties properties = new Properties();
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            inputStream = loader.getResourceAsStream("project.properties");
+            properties.load(inputStream);
+
+            properties.forEach(
+                             (key, value)
+                                ->
+                            System.out.println(
+                                    key + "=" + value
+                            )
+                     );
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+Read property file from resource directory using java (with example)
+
+1. Given a property file defining the properties of an application.
+2. Property file will be residing in the resource folder.
+3. We will load the property file.
+4. We will get hashtable (key / value pairs) of application’s properties.
+5. We will iterate through the properties and print all properties.
